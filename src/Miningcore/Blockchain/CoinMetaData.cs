3fbeb397907851859bad1545a8e42634e56d6d0c
Giwayla <dennylijun@163.com>
2021-08-03 15:52:17 +0800

    first

---
+++ src/Miningcore/Blockchain/CoinMetaData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Miningcore.Blockchain
+{
+    public static class CoinMetaData
+    {
+        public const string BlockHeightPH = "$height$";
+        public const string BlockHashPH = "$hash$";
+    }
+}
